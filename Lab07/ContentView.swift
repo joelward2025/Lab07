0d8d49c88025a6adfe30bf9131aa87f5466c017d
joelward2025 <joelward@cedarville.edu>
2024-10-04 14:30:25 -0400

    Initial Commit

---
+++ Lab07/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab07
+//
+//  Created by Joel Ward on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab07/Lab07App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab07App.swift
+//  Lab07
+//
+//  Created by Joel Ward on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
