cd829d60e7fcc8dc9b2a397901e6302f33336c3a
joelward2025 <joelward@cedarville.edu>
2024-11-28 20:37:36 -0800

    horizontal padding

--- Lab07/ContentView.swift
+++ Lab07/ContentView.swift
@@ -15,23 +15,30 @@ struct ArticleView: View {
                 Image(news.model.imageName)
                     .resizable()
                     .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                Spacer()
                 Text(news.model.category)
                     .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                 HStack{
                     Text(news.model.title)
                         .font(.largeTitle)
                         .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
                     Image(systemName: news.model.isFavorite ? "heart.fill" : "heart")
                         .foregroundStyle(.red)
                         .font(.system(size: 0.1 * geometry.size.width))
                         .onTapGesture{
                             news.intent()
                         }
+                        .padding(.horizontal)
                 }
+                Spacer()
                 Text(news.model.text)
+                    .padding(.horizontal)
                 HStack{
                     Link("More...", destination: URL(string: news.model.url)!)
+                        .padding(.horizontal)
 
                     Spacer()
                 }
